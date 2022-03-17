7c5d8f9d5fb301dd0e7cbf89817e996a2d1cf6ac
ilongge <1015820857@qq.com>
2022-03-17 10:34:05 +0800

    Site updated: 2022-03-17 10:34:02

---
+++ WNPlayer-ilongge.framework/Headers/WNPlayer-ilongge.h
@@ -0,0 +1,17 @@
+//
+//  WNPlayer-ilongge.h
+//  Pods
+//
+//  Created by ilongge on 2022/3/16.
+//
+
+#ifndef WNPlayer_ilongge_h
+#define WNPlayer_ilongge_h
+
+#import "WNPlayer.h"
+#import "WNControlView.h"
+#import "WNControlViewProtocol.h"
+#import "WNDisplayView.h"
+#import "WNPlayerManager.h"
+
+#endif /* WNPlayer_ilongge_h */
